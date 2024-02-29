28a451a57053f91d23cc2856a3ab1d07fe1e7583
mayarakok <akokmayar607@gmail.com>
2024-02-29 20:47:26 +0300

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
-
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+greet_programmer()    
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Alice")    
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default()
+greet_with_default("Bob")    
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+result = add(3, 5)
+print("Result of addition:", result)
 
 def halve(number):
-    pass
+    return number / 2
+halved_value = halve(10)
+print("Halved value:", halved_value)
+
+
+
